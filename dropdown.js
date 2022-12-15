4ffa36d27382aab70e628683058e3496cc58645e
NabiShirinov <117586522+NabiShirinov@users.noreply.github.com>
2022-12-16 02:10:54 +0400

    update

---
+++ dropdown.js
@@ -0,0 +1,9 @@
+function dropdown() {
+    var dropdown = document.getElementById("dropdown");
+    if (dropdown.style.display == "none") {
+        dropdown.style.display = "flex";
+    } 
+    else {
+        dropdown.style.display = "none";
+    }
+  }
